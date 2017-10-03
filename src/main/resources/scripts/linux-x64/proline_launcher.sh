d4ea563b6e1cc635aafe46e82fb6f67caff301f2
AlexandreBurel <alexandre.burel@unistra.fr>
2017-10-03 13:23:08 +0000

    Update of the launcher script, it should be fully portable now Also added a ini file, same structure as the windows version

--- src/main/resources/scripts/linux-x64/proline_launcher.sh
+++ src/main/resources/scripts/linux-x64/proline_launcher.sh
@@ -1,64 +1,99 @@
-#!/bin/sh
+#!/bin/bash
 
 cd -- "$(dirname "$0")"
 
-# define local path
-export CORTEX=Proline-Cortex-0.4.0-SNAPSHOT
-export STUDIO=ProlineStudio-1.2.0-SNAPSHOT
-# warning: other scripts are using java, make sure they use $JAVA instead of `java` (or at least use the given JAVA_HOME variable)
-export JAVA_HOME=$(pwd)/$STUDIO/jre
-export JAVA=$JAVA_HOME/bin/java
+
+# read ini file
+echo "***********************************************************************************"
+export INI_FILE=proline_launcher.ini
+echo "Read file $INI_FILE"
+while IFS='= ' read var val
+do
+  if [[ $var == \[*] ]]; then
+    section=$var
+  elif [[ $val ]]; then
+    # remove \r if any
+    val=`echo $val|sed 's/\r//'`
+    declare "$var$section=$val"
+  fi
+done < $INI_FILE
+
+# set absolute directory pathes
+export PROLINE_ZERO_HOME=$(pwd)
+export CORTEX_NAME=Proline-Cortex-${cortex_version}
+export CORTEX_HOME=$PROLINE_ZERO_HOME/$CORTEX_NAME
+export HORNETQ_HOME=$CORTEX_HOME/hornetq_light-$hornetq_version
+export HORNETQ_CONFIG_DIR=$HORNETQ_HOME/config/stand-alone/non-clustered
+export STUDIO_HOME=$PROLINE_ZERO_HOME/ProlineStudio-$studio_version
+export JAVA=$PROLINE_ZERO_HOME/$java_home/bin/java
+
+# make sure java is executable
 chmod +x $JAVA
-#if test -z "$JAVA_HOME" ; then
-#  JAVA=java
-#else
-#  JAVA="$JAVA_HOME/bin/java"
-#fi
 
 
 # start H2
-$JAVA -cp "$CORTEX/lib/*" org.h2.tools.Console -tcp -pg & pidh2=$!
+echo "***********************************************************************************"
+echo "Start H2"
+$JAVA -cp "$CORTEX_HOME/lib/*" org.h2.tools.Console -tcp -pg & pidh2=$!
+
 
 # setup databases if needed
-if [ ! -f setup.done ]; then
+if [ ! -f $PROLINE_ZERO_HOME/setup.done ]; then
 
-  cd $CORTEX
+  echo "***********************************************************************************"
+  echo "First run: setup databases"
+  cd $CORTEX_HOME
   $JAVA -Xmx1024m -cp "lib/*:config" fr.proline.admin.RunCommand setup
   $JAVA -Xmx1024m -cp "lib/*:config" fr.proline.admin.RunCommand create_user -l proline -p proline
 
-  cd ..
-  touch setup.done
+  touch $PROLINE_ZERO_HOME/setup.done
 fi
 
+
 # start hornetq
-cd $CORTEX/hornetq_light-2.4.0.Final/bin
-chmod +x *.sh
-./run.sh & pidhq=$!
+echo "***********************************************************************************"
+echo "Start HornetQ"
+cd $HORNETQ_HOME/bin
+
+export CLUSTER_PROPS="-Djnp.port=1099 -Djnp.rmiPort=1098 -Djnp.host=localhost -Dhornetq.remoting.netty.host=0.0.0.0 -Dhornetq.remoting.netty.port=5445"
+export JVM_ARGS="$CLUSTER_PROPS -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms512M -Xmx1024M -Dhornetq.config.dir=$HORNETQ_CONFIG_DIR -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=file://$HORNETQ_CONFIG_DIR/logging.properties -Djava.library.path=./lib/linux-i686:./lib/linux-x86_64"
+export CLASSPATH=$HORNETQ_CONFIG_DIR:../schemas/
+for i in `ls ../lib/*.jar`; do
+	CLASSPATH=$i:$CLASSPATH
+done
+$JAVA $JVM_ARGS -classpath $CLASSPATH -Dcom.sun.management.jmxremote org.hornetq.integration.bootstrap.HornetQBootstrapServer hornetq-beans.xml &
 sleep 5
-cd ../../..
+
 
 # start cortex
-cd $CORTEX
+echo "***********************************************************************************"
+echo "Start Proline-Cortex"
+cd $CORTEX_HOME
 chmod +x *.sh
-./start_cortex.sh & pidpc=$!
-# catch cortex real pid otherwise it may not stop by itself
-pid_cortex=$(pgrep -P $pidpc)
+export LANG=en_US.UTF-8
+$JAVA -Xmx4G -XX:+UseG1GC -XX:+UseStringDeduplication -XX:MinHeapFreeRatio=10 -XX:MaxHeapFreeRatio=30 -cp "config:Proline-Cortex-$cortex_version.jar:lib/*" -Dlogback.configurationFile=config/logback.xml fr.proline.cortex.ProcessingNode & pid_cortex=$!
 sleep 5
-cd ..
+
 
 # start studio
-cd $STUDIO/bin/
+echo "***********************************************************************************"
+echo "Start Proline Studio"
+cd $STUDIO_HOME/bin
 chmod +x prolinestudio
 ./prolinestudio
-cd ../..
 
+
+# stop all and quit
+echo "***********************************************************************************"
+echo "Stop all processes and quit"
 # stop cortex
 kill -9 $pidpc $pid_cortex
 # stop hornetq
-cd $CORTEX/hornetq_light-2.4.0.Final/bin
-./stop.sh
-cd ../..
+touch $HORNETQ_CONFIG_DIR/STOP_ME
 # stop h2
 kill -9 $pidh2
 sleep 5
+echo "***********************************************************************************"
+
+
 
--- src/main/resources/scripts/linux-x64/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-java -cp "Proline-Cortex-${cortex.version}/lib/*" org.h2.tools.Console -tcp -pg & pid=$!
-
-cd Proline-Cortex-${cortex.version}
-java -Xmx1024m -cp "lib/*;config" fr.proline.admin.RunCommand setup
-java -Xmx1024m -cp "lib/*;config" fr.proline.admin.RunCommand create_user -l proline -p proline
-
-(sleep 5 && kill -9 $pid) &
